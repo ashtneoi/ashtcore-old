3d83de9d637d771ec8e25e5bd3ce1d1528049f11
ashtneoi <ashtneoi@gmail.com>
2020-02-24 00:01:42 -0800

    Add initial ideas for bin and text writers

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![no_std]
+
+pub trait Write<T> { }
+
+pub trait WriteText { }
+
+pub struct Utf8TextWriter<W: Write<u8>> {
+    writer: W,
 }
+
+impl<W: Write<u8>> WriteText for Utf8TextWriter<W> { }
